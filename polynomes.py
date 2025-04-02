5d71385aac6d85467d5ee87b6e98a9ca57d01a31
Martin <martin@MacBook-Air-de-Martin.local>
2025-04-02 14:46:47 +0200

    TD2 v2

--- polynomes.py
+++ polynomes.py
@@ -92,6 +92,46 @@ class PolynomialZq:
 				NP[i+j]+=p[i]*q[j]
 		return PolynomialZq(NP, self.getInfos()[0], self.getInfos()[1])
 
+	def opp(self):
+		NP = [0 for i in range(self.n)]
+		for i in range(len(self.poly)):
+			NP[i] -= self.poly[i]
+		return PolynomialZq([coeff % self.q for coeff in NP], self.q, self.n)
+
+	def mulKaratsuba(self, Q):
+
+		assert(self.getInfos() == Q.getInfos())
+		
+		if len(self.poly) == 1:
+			return self.poly[0]*Q.poly[0]
+		else:
+			p = self.poly
+			q = Q.getPoly()
+			d = len(p)
+			
+			i,n = self.getInfos()
+			if d%2!=0:
+				p,q = p+[0],q+[0]
+				d+=1
+			l = d/2
+			p0 = PolynomialZq(self.poly[:l], i,l)
+			p1 = PolynomialZq(self.poly[l:], i,l)
+			q0 = PolynomialZq(Q.poly[:l], i,l)
+			q1 = PolynomialZq(Q.poly[l:], i,l)
+			p0q0 = p0.mulKaratsuba(q0)
+			p1q1 = p1.mulKaratsuba(q1)
+
+			prod = p0.add(q1).mulKaratsuba(q0.add(q1)).add(p0q0.add(p1q1).opp())
+			p1q1 = PolynomialZq([0 for i in range(2*l)]+p1q1.getPoly(), i, 4*l)
+			prod = PolynomialZq([0 for i in range(l)]+prod.getPoly(), i, 4*l)
+			return p0q0.add(prod).add(p1q1)
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
 
@@ -108,7 +148,9 @@ if __name__ == '__main__':
 
 	# test pour saboir si le modulo des polynomes fonctionne bien
 	assert(PolynomialZq([2,5,7,3,5], 3, 3).__str__() == "1*X^2 + 1*X^1 + 0")
+
 	### EXECICE 5
+
 	assert(PolynomialZq([0,0,2],4,4).mul(PolynomialZq([0,0,5,3],4,4)).__str__() == 0*X^3 + 0*X^2 + 2*X^1 + 2)
 	
 
